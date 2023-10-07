0f064e1eed6639e172c6a6e35dd54dd9a288dcf7
Ahmed <shiekhahmed135@gmail.com>
2023-10-08 01:52:24 +0500

    learning api

--- content/api/urls.py
+++ content/api/urls.py
@@ -7,5 +7,6 @@ router.register(r'project', ProjectViewSet, basename="project")
 router.register(r'technology', TechnologyViewSet, basename="technology")
 router.register(r'about', AboutViewSet, basename='about')
 router.register(r'contact', ContactViewSets, basename="contact")
+router.register(r'learning', LearningViewSets, basename="learning")
 
 urlpatterns = router.urls
